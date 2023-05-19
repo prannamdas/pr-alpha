578a4c724c61f34f4cbe76965741b7e5e2ef9d3f
prannamdas <131272269+prannamdas@users.noreply.github.com>
2023-05-18 22:48:04 -0400

    Update build-customAMI.sh

--- build-customAMI.sh
+++ build-customAMI.sh
@@ -7,5 +7,5 @@ sudo apt-get update -y
 sudo apt-get install tomcat9-admin tomcat9-common -y
 sudo apt-get install tomcat9 -y
 cd /var/lib/tomcat9/webapps/
-sudo wget https://buildjava.s3.us-east-2.amazonaws.com/alpha.war
+sudo wget https://output-maven.s3.us-east-2.amazonaws.com/alpha.war
 sudo systemctl start tomcat9
